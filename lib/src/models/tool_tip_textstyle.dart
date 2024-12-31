8d2883b1375eeecd7602e933f0b3f0426ebdc6b8
alokjha2 <alokj4702@gmail.com>
2024-12-31 21:27:16 +0530

    added tooltipTextStyle

---
+++ lib/src/models/tool_tip_textstyle.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ToolTipTextStyle {
+  final Color fontColor;
+  final EdgeInsets padding;
+
+
+  const ToolTipTextStyle({
+    this.fontColor = Colors.black,
+    this.padding = const EdgeInsets.all(8.0),
+  });
+}
